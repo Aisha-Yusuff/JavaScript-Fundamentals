aa425a990179d9363b6d9adc34198add0bebd0e4
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-21 17:55:49 +0100

    Completed callback exercise and challenges

--- Phase-One/8_callbacks/counter.js
+++ Phase-One/8_callbacks/counter.js
@@ -1,8 +1,14 @@
 let counter = 0;
 
 const increment = () => {
-  counter++;
   console.log(counter);
+  counter++;
+
+  if (counter === 11) {
+    clearInterval(counting);
+  }
 };
 
-setInterval(increment, 1000);
+// the if statement here is what we use to break the interval
+
+const counting = setInterval(increment, 1000);
