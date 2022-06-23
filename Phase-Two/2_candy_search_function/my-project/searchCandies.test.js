5f4b2b00a5f21b973975de7a1b2a05460b495618
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-23 18:21:58 +0100

    Passed third test

--- Phase-Two/2_candy_search_function/my-project/searchCandies.test.js
+++ Phase-Two/2_candy_search_function/my-project/searchCandies.test.js
@@ -8,4 +8,12 @@ describe("searchCandies", () => {
   it("it returns candies that start with Ma and their price is lower than 2", () => {
     expect(searchCandies("Ma", 2)).toEqual(["Mars"]);
   });
+
+  it("it returns candies that start with S and their price is lower than 10", () => {
+    expect(searchCandies("S", 10)).toEqual([
+      "Skitties",
+      "Skittles",
+      "Starburst",
+    ]);
+  });
 });
