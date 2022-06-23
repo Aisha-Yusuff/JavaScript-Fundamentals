11da26edf450ecc6cbfd5393efe708e46b2e19f5
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-23 18:20:07 +0100

    Passed second test

--- Phase-Two/2_candy_search_function/my-project/searchCandies.test.js
+++ Phase-Two/2_candy_search_function/my-project/searchCandies.test.js
@@ -1,7 +1,11 @@
 const searchCandies = require("./searchCandies");
 
 describe("searchCandies", () => {
-  it("returns candies that start with ma and have a price lower than 10", () => {
+  it("it returns candies that start with Ma and have a price lower than 10", () => {
     expect(searchCandies("Ma", 10)).toEqual(["Mars", "Maltesers"]);
+  });
+
+  it("it returns candies that start with Ma and their price is lower than 2", () => {
+    expect(searchCandies("Ma", 2)).toEqual(["Mars"]);
   });
 });
